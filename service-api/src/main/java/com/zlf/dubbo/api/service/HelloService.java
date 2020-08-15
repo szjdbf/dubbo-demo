94bdcc140aac599e1ed9028c214776eed7dae14d
zhanglifang <zhanglf@chinacarbon-al.com>
2020-08-15 09:48:59 +0800

    dubbo消费者与提供者

---
+++ service-api/src/main/java/com/zlf/dubbo/api/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.zlf.dubbo.api.service;
+
+/**
+ * @author zhanglifang
+ * @date 2020/8/15
+ */
+public interface HelloService {
+
+    void sayHello();
+
+}
---
+++ service-provider/src/main/java/com/zlf/dubbo/provider/service/impl/HelloServiceImpl.java
@@ -0,0 +1,19 @@
+package com.zlf.dubbo.provider.service.impl;
+
+import com.zlf.dubbo.api.service.HelloService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhanglifang
+ * @date 2020/8/15
+ */
+@Service("helloService")
+public class HelloServiceImpl implements HelloService {
+
+
+    @Override
+    public void sayHello() {
+        System.out.println("你好，世界");
+    }
+
+}
